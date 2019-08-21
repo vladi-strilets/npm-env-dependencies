c320d45659b4c57202fec7701623cbf966c324cf
vladi_strilets <vladi.strilets@gmail.com>
2019-08-21 18:56:35 +0200

    v0.0.2

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 #! /usr/bin/env node
 var path = require("path");
-var pkg = require(path.join(process.cwd(), "package.json"));
+var package = require(path.join(process.cwd(), "package.json"));
 var log = console.log;
 
 var spawn = require("cross-spawn");
 
-var env = process.argv.slice(2);
+var currentEnv = process.argv.slice(2);
 
-var dependenciesEnv = pkg[env];
+var dependenciesCurrentEnv = package["npm-env"][currentEnv];
 
-// install dependencies
-if (dependenciesEnv && Object.keys(dependenciesEnv).length) {
-	log("Installing dependencies for " + env);
+if (dependenciesCurrentEnv && Object.keys(dependenciesCurrentEnv).length) {
+	// uninstall dependencies from other envs
+	for (var otherEnv in package["npm-env"]) {
+		if (otherEnv != currentEnv) {
+			log("Uninstalling unused dependencies from " + otherEnv);
+			var npmArgs = ["uninstall"];
+			for (var dep in package["npm-env"][otherEnv]) {
+				if (package["npm-env"][otherEnv].hasOwnProperty(dep)) {
+					if (!dependenciesCurrentEnv.hasOwnProperty(dep)) {
+						if (package["dependencies"].hasOwnProperty(dep))
+							npmArgs.push(dep);
+					}
+				}
+			}
+			var options = {
+				stdio: "inherit"
+			};
+			log(npmArgs);
+			spawn.sync("npm", npmArgs, options);
+		}
+	}
+	// install dependencies from the current env
+	log("Installing dependencies & updating versions for " + currentEnv);
 	var npmArgs = ["install"];
-	for (var dep in dependenciesEnv) {
-		if (dependenciesEnv.hasOwnProperty(dep)) {
-			npmArgs.push(dep.concat("@").concat(dependenciesEnv[dep]));
+	for (var dep in dependenciesCurrentEnv) {
+		if (dependenciesCurrentEnv.hasOwnProperty(dep)) {
+			npmArgs.push(dep.concat("@").concat(dependenciesCurrentEnv[dep]));
 		}
 	}
 	var options = {
 		stdio: "inherit"
 	};
-	spawn("npm", npmArgs, options);
+	log(npmArgs);
+	spawn.sync("npm", npmArgs, options);
 } else {
-	log("No specific dependencies on " + env);
+	log("No specific dependencies on " + currentEnv);
 }
